e91a0cbde0eef02f833f994da1e16032e608ae2c
Gabriel <77220208+ghithy@users.noreply.github.com>
2025-12-15 10:15:11 -0300

    ...

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <msp430.h>
-#include <stdio.h>
 
 #define DIGIT0 0x01
 #define DIGIT1 0x02
@@ -18,16 +17,17 @@
 
 #define CELSIUS 1
 
+#define ADC_SAMPLES 64
+
 //============================================
 
-unsigned char i;
-
-unsigned int adc_buffer[64];
+unsigned int adc_buffer[ADC_SAMPLES];
 unsigned int adc_avg = 0;
-unsigned long sum = 0;
+unsigned long adc_sum = 0;
 
-volatile char tx_string[32];
+char tx_string[32];
 volatile unsigned char tx_index = 0;
+volatile unsigned char uart_tx_busy = 0;
 
 unsigned char temp_unit = CELSIUS;
 unsigned int temp_c = 0;
@@ -36,6 +36,10 @@ unsigned char digit3 = 0;
 unsigned char digit2 = 0;
 unsigned char digit1 = 0;
 unsigned char digit0 = 0;
+
+volatile unsigned long ta1_overflows;
+
+volatile unsigned char new_sample = 0;
 
 //============================================
 
@@ -51,11 +55,20 @@ void MAX7219_config(void);
 void MAX7219_SPI_TX(unsigned char reg, unsigned char data);
 
 void get_temp_digits(unsigned int temp);
+unsigned long get_timestamp_ticks(void);
+
+
+void build_temp_string(unsigned char hh,
+                       unsigned char mm,
+                       unsigned char ss,
+                       char unit_char);
 
 
 //============================================
 void main(void) {
   WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer
+
+  //----------------------------------
 
   GPIO_config();
   clocks_config();
@@ -68,8 +81,64 @@ void main(void) {
 
   __enable_interrupt();
  
+  //----------------------------------
+
   while (1) {
 
+    if (new_sample && !uart_tx_busy) {
+      unsigned int temp = 0;
+      unsigned char temp_unit_display = 0;
+      char temp_unit_char = ' ';
+
+      unsigned long ts = get_timestamp_ticks();
+      unsigned long seconds = ts >> 15;
+      unsigned int hh = seconds / 3600;
+      unsigned int mm = (seconds % 3600) / 60;
+      unsigned int ss = seconds % 60;
+
+      new_sample = 0;
+      
+      //----------------------------------
+
+      if (temp_unit == CELSIUS) {
+        temp = temp_c;
+        temp_unit_display = 0b01001110;
+        temp_unit_char = 'C';
+
+        P1OUT &= ~BIT0;
+        P1OUT |= BIT6;
+      } else {
+        temp_f = (temp_c * 9) / 5 + 3200;
+        temp = temp_f;
+        temp_unit_display = 0b01000111;
+        temp_unit_char = 'F';
+
+        P1OUT &= ~BIT6;
+        P1OUT |= BIT0;
+      }
+ 
+      get_temp_digits(temp);
+    
+      //----------------------------------
+
+      MAX7219_SPI_TX(DIGIT0, temp_unit_display);
+      MAX7219_SPI_TX(DIGIT5, digit3);
+      MAX7219_SPI_TX(DIGIT4, digit2 + 0b10000000);
+      MAX7219_SPI_TX(DIGIT3, digit1);
+      MAX7219_SPI_TX(DIGIT2, digit0);
+
+      //----------------------------------
+      
+      build_temp_string(hh, mm, ss, temp_unit_char);
+
+      tx_index = 0;
+      uart_tx_busy = 1;
+
+      UCA0TXBUF = tx_string[tx_index++];
+
+      IE2 |= UCA0TXIE;
+      // IFG2 |= UCA0TXIFG;
+    }
   }
 }
 
@@ -84,7 +153,7 @@ void GPIO_config(void) {
   P1IES = BIT3;
   P1IE = BIT3;
    
-  P2DIR = 0xff;
+  P2DIR = 0xFF;
   P2OUT = BIT0; // SPI CS/SS pin = HIGH
 }
 
@@ -107,7 +176,6 @@ void TA0_config(void) {
   // Timer0 CLK = ACLK
   TA0CTL = TASSEL0 + MC0;
   TA0CCTL1 = OUTMOD0 + OUTMOD1 + OUTMOD2 + OUT;
-  // TA0CCTL0 = CCIE;
   TA0CCR0 = 32767; // PWM period = sample-and-conversion period
   TA0CCR1 = 16383; // PWM duty cycle = 50 %
 }
@@ -115,10 +183,8 @@ void TA0_config(void) {
 
 //============================================
 void TA1_config(void) {
-  // Timer1 CLK = MCLK @ 4 MHz
-  TA1CTL = TASSEL1;
-  TA1CCR0 = 800; // 20 ms debouncer
-  TA1CCTL0 = CCIE;
+  // Timer1 CLK = ACLK
+  TA1CTL = TASSEL0 + MC1 + TAIE;
 }
 
 
@@ -144,7 +210,7 @@ void ADC10_config(void) {
 
   ADC10AE0 = BIT4;
 
-  ADC10DTC1 = 64; // samples
+  ADC10DTC1 = ADC_SAMPLES;
   ADC10SA = (unsigned int)&adc_buffer[0];
 
   ADC10CTL0 |= ENC;
@@ -167,7 +233,7 @@ void UART_config(void) {
   
   IFG2 &= ~UCA0TXIFG;
   
-  IE2 = UCA0TXIE;
+  // IE2 |= UCA0TXIE;
 }
 
 
@@ -200,7 +266,7 @@ void MAX7219_config(void) {
   MAX7219_SPI_TX(DECODE_MODE, 0b00111110);
   MAX7219_SPI_TX(INTENSITY, 0x03);
 
-  for (int i = 1; i <= 8; i++)
+  for (unsigned char i = 1; i <= 8; i++)
     MAX7219_SPI_TX(i, 0x0F);   // blank (code B: 0xF = blank)
 }
 
@@ -220,77 +286,31 @@ void MAX7219_SPI_TX(unsigned char reg, unsigned char data) {
 
 
 //============================================
-// __attribute__((interrupt(TIMER0_A0_VECTOR)))
-// void TA0_ISR(void) {
-// #pragma vector=TIMER0_A0_VECTOR
-// __interrupt void timer_isr(void) {
-  // P1OUT ^= BIT0;
-// }
-
-
-//============================================
 __attribute__((interrupt(ADC10_VECTOR)))
 void ADC10_ISR(void) { 
-  unsigned int temp = 0;
-  unsigned char unit_display = 0;
-  char unit_char = "";
   
   ADC10CTL0 &= ~(ENC + MSC);
 
-  // ----------------------------------
-  sum = 0;
+  new_sample = 1;  
+  adc_sum = 0;
   
-  for(i = 0; i < 64; i++) {
-    sum += adc_buffer[i];
+  //----------------------------------
+
+  for(unsigned int i = 0; i < ADC_SAMPLES; i++) {
+    adc_sum += adc_buffer[i];
   }
   
-  adc_avg = sum >> 6;
+  adc_avg = adc_sum >> 6;
+
+  //----------------------------------
 
   // temp = (adc_avg / 1023.0 * 1.5 - 0.5) * 100; -> float
   temp_c = ((adc_avg * 15000L) / 1023 - 5000); // first 4 figures
 
-  if (temp_unit == CELSIUS) {
-    temp = temp_c;
-    unit_display = 0b01001110;
-    unit_char = 'C';
-
-    P1OUT &= ~BIT0;
-    P1OUT |= BIT6;
-  } else {
-    temp_f = (temp_c * 9) / 5 + 3200;
-    temp = temp_f;
-    unit_display = 0b01000111;
-    unit_char = 'F';
-
-    P1OUT &= ~BIT6;
-    P1OUT |= BIT0;
-  }
- 
-  get_temp_digits(temp);
-
-  // ----------------------------------
-  MAX7219_SPI_TX(DIGIT0, unit_display);
-  MAX7219_SPI_TX(DIGIT5, digit3);
-  MAX7219_SPI_TX(DIGIT4, digit2 + 0b10000000);
-  MAX7219_SPI_TX(DIGIT3, digit1);
-  MAX7219_SPI_TX(DIGIT2, digit0);
-      
-  // ----------------------------------
-  sprintf(&tx_string[0],
-          "Temp = %u%u.%u%u %c\n\r",
-          (unsigned char)digit3,
-          (unsigned char)digit2,
-          (unsigned char)digit1,
-          (unsigned char)digit0,
-          (char)unit_char);
-
-  tx_index = 0;
-  IE2 |= UCA0TXIE;
-  IFG2 |= UCA0TXIFG;
-  // ----------------------------------
+  //----------------------------------
 
   ADC10SA = (unsigned int)&adc_buffer[0];
-  ADC10DTC1 = 64;
+  ADC10DTC1 = ADC_SAMPLES;
 
   ADC10CTL0 |= ENC + MSC;
 }
@@ -302,6 +322,7 @@ void UART_TX_ISR(void) {
   if (tx_string[tx_index] == '\0') {
 		IE2 &= ~UCA0TXIE;
 		tx_index = 0;
+		uart_tx_busy = 0;
 	} else {
 		UCA0TXBUF = tx_string[tx_index];
 		tx_index++;
@@ -316,22 +337,43 @@ void port1_ISR(void) {
     P1IFG &= ~BIT3;
     P1IE &= ~BIT3;
 
-    TA1CTL |= MC0;
+    // Start 20 ms (655 ticks) debouncer
+    // relative to current timer count.
+    // Interrupt fires when TA1R = TA1R + 655
+    TA1CCTL0 = CCIE;
+    TA1CCR0 = TA1R + 655;
   }
 }
 
 
 //============================================
 __attribute__((interrupt(TIMER1_A0_VECTOR)))
-void TA1_ISR(void) {
-  TA1CTL &= ~MC0;
+void TA1_CCR0_ISR(void) {
+  TA1CCTL0 &= ~CCIE;
 
   if ((~P1IN) & BIT3) {
     temp_unit ^= 1;
+    // temp_unit initially defined as 1
+    // so temp_unit = 1 => 1 ^ 1 => temp_unit = 0.
+    // temp_unit = 0 => 0 ^ 1 => temp_unit = 1.
   }
 
   P1IFG &= ~BIT3;
   P1IE |= BIT3;
+}
+
+
+//============================================
+__attribute__((interrupt(TIMER1_A1_VECTOR)))
+void TA1_ISR(void) {
+  switch (TA1IV) {
+    case TA1IV_TAIFG:
+      ta1_overflows++;
+      break;
+
+    default:
+      break;
+  }
 }
 
 
@@ -347,3 +389,49 @@ void get_temp_digits(unsigned int temp) {
 
 
 //============================================
+unsigned long get_timestamp_ticks(void) {
+  unsigned long ovf;
+  unsigned int tar;
+
+  ovf = ta1_overflows;
+  tar  = TA1R;
+
+  return (ovf << 16) | tar;
+}
+
+
+//============================================
+void build_temp_string(unsigned char hh,
+                       unsigned char mm,
+                       unsigned char ss,
+                       char unit_char) {
+  tx_string[0]  = '[';
+  tx_string[1]  = (hh / 10) + '0';
+  tx_string[2]  = (hh % 10) + '0';
+  tx_string[3]  = ':';
+  tx_string[4]  = (mm / 10) + '0';
+  tx_string[5]  = (mm % 10) + '0';
+  tx_string[6]  = ':';
+  tx_string[7]  = (ss / 10) + '0';
+  tx_string[8]  = (ss % 10) + '0';
+  tx_string[9]  = ']';
+  tx_string[10] = ' ';
+  tx_string[11] = 'T';
+  tx_string[12] = 'e';
+  tx_string[13] = 'm';
+  tx_string[14] = 'p';
+  tx_string[15] = ' ';
+  tx_string[16] = '=';
+  tx_string[17] = ' ';
+  tx_string[18] = digit3 + '0';
+  tx_string[19] = digit2 + '0';
+  tx_string[20] = '.';
+  tx_string[21] = digit1 + '0';
+  tx_string[22] = digit0 + '0';
+  tx_string[23] = ' ';
+  tx_string[24] = unit_char;
+  tx_string[25] = '\r';
+  tx_string[26] = '\n';
+  tx_string[27] = '\0';
+}
+
